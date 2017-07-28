912c30a99e532a29171b2fe040321e88eda7c022
johnnymoctezuma <johnnymocc@gmail.com>
2017-07-28 00:56:27 -0700

    updated javascript

--- assets/app.js
+++ assets/app.js
@@ -1,35 +1,38 @@
 var topics = ["games", "movies", "reactions", "pokemon", "trending"];
 
-function displayGifs(){
+showButtons();
 
-  var buttonText = $(this).attr("data-name");
-  var api = "6e5baa3647a549e18bc64f6bf6e79fd8"
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=" + api;
-
-
+$(document).on("click", "topics", function(event){
+  var buttonText = $(this).attr("data-topics");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=6e5baa3647a549e18bc64f6bf6e79fd8";
+console.log(buttonText);
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response){
+  })
+  .done(function(response){
   var gifs = response.data;
   for(var i = 0; i < gifs.length; i++){
-    $("#showGifs").append('<img src="'+ gifs[i].images.fixed_width.url + '"/>');
-    // console.log(response)
+    var gifDiv = $("<div class='item'>");
+    var gifImage = $("<img>");
+    gifImage.attr("src",
+  gifs[i].images.fixed_height.url);
+
+  gifDiv.prepend(gifImage);
+
+  $("showGifs").append(gifDiv);
 }
-})
-}
+});
+});
+
 function showButtons(){
   $("#buttons").empty();
   for(var i = 0; i < topics.length; i++){
     var a = $("<button>");
     a.addClass("topics");
-    a.attr("data-name", topics[i]);
+    a.attr("buttons", topics[i]);
     a.text(topics[i]);
     $("#buttons").append(a);
+
   }
 }
-
-showButtons()
-$("#buttons").on("click", function(event){
-console.log(event)
-})
